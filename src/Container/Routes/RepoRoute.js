88025488340c520729283369d34c71434d033bdd
farhad taran <farhad86@gmail.com>
2020-04-18 02:22:41 +0100

    remove markdown js

--- src/Container/Routes/RepoRoute.js
+++ src/Container/Routes/RepoRoute.js
@@ -3,13 +3,14 @@ import axios from'axios'
 import './RepoRoute.css'
 import {connect} from 'react-redux'
 import Spinner from './Spinner'
-import Markdown from './Markdown'
+import ReactMarkdown from 'react-markdown'
 
 
 
 class Repo extends Component {
 
     state={
+        showMarkDown: false,
         readMe:'',
         repos:this.props.repos,
         src:'',
@@ -27,21 +28,18 @@ class Repo extends Component {
             src:fetchedRepo.html_url,
             created:fetchedRepo.created_at,
             updated:fetchedRepo.updated_at,
-            
+            showMarkDown: false,
         })
 
         if(fetchedRepo.isRepo){
-            axios.get(`${fetchedRepo.url}/readme`)
-            .then(response => {
-                axios.get(`${response.data.download_url}`)
-                .then(res => {
-                    this.props.hideSpinner();
-                    this.props.showMarkDown();
-                    this.setState({
-                        readMe:res.data,
-                    })
-                })
-                
+            debugger
+            axios.get(fetchedRepo.readMe)
+            .then(res => {
+                this.props.hideSpinner();
+                this.setState({
+                    readMe:res.data,
+                    showMarkDown: true
+                })                
             })
          }
 
@@ -56,17 +54,18 @@ class Repo extends Component {
             src:fetchedRepo.html_url,
             created:fetchedRepo.created_at,
             updated:fetchedRepo.updated_at,
-            
+            showMarkDown: false            
         })
+
         if(fetchedRepo.isGist){
             Object.keys(fetchedRepo.files).forEach(key => {                    
                         if(key.includes('.md')){
                             axios.get(fetchedRepo.files[key].raw_url)                            
                            .then(res => {
                                this.props.hideSpinner();
-                               this.props.showMarkDown();
                                this.setState({
-                                   readMe:res.data
+                                   readMe:res.data,
+                                   showMarkDown: true
                                })
                            })
                             
@@ -83,29 +82,23 @@ class Repo extends Component {
         if(this.props.repos.length == 0 ){
             this.props.history.replace('/about')
         }
-
         else {
             this.fetchRepo(this.props)
             this.fetchGist(this.props)
-        }
-    
+        }    
     }
 
     componentWillReceiveProps(nextProps){
-
         this.fetchRepo(nextProps)
-        this.fetchGist(nextProps)
-               
+        this.fetchGist(nextProps)               
      }
 
- 
-
     render(){
-        
+        let style = this.state.showMarkDown ? {visibility:'visible',opacity:'1',transition:'all 0.3s'} : {visibility:'hidden',opacity:'0',transition:'all 0.3s'} 
         return(
-            <div className="repoRoute">
+            <div className="repoRoute" style={style}>
                 <Spinner />
-                <Markdown source={this.state.readMe} />
+                <ReactMarkdown source={this.state.readMe} escapeHtml={true} />
                 <p className="link-wrap"> created at {this.state.created} / updated at {this.state.updated} <br/> 
                 for more information <a target="_blank" href={this.state.src}  className="link" >Click Here</a> </p>
             </div>
@@ -122,8 +115,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        hideSpinner: ()=> dispatch({type:'hideSpinner'}),
-        showMarkDown: ()=> dispatch({type:'showMarkDown'})
+        hideSpinner: ()=> dispatch({type:'hideSpinner'})
     }
 }
 
