77799d840966d771a30cd3274592017c7ead5d7e
FARHAD TARAN <farhadtaran@FARHADs-MacBook-Pro.local>
2024-10-30 11:22:01 +0000

    Updates

---
+++ precache-manifest.1d0633d2b74e44e6f8c22610aec92c72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3522de573f1af54bb37ea63ad996ee9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd98b09c46091b8ed57a",
+    "url": "/static/css/main.01ba3685.chunk.css"
+  },
+  {
+    "revision": "acbacc5c0b9c1b377e2a",
+    "url": "/static/js/2.fad6da8c.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.fad6da8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd98b09c46091b8ed57a",
+    "url": "/static/js/main.7411a946.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "383539793be80c557effccee1e430e57",
+    "url": "/static/media/icon-2.38353979.svg"
+  },
+  {
+    "revision": "4083b0bd6ba50b69f85094b16b863e75",
+    "url": "/static/media/icon.4083b0bd.svg"
+  },
+  {
+    "revision": "907424c5353e469fa50f33a1caddc5b1",
+    "url": "/static/media/profilePicture.907424c5.jpg"
+  }
+]);
